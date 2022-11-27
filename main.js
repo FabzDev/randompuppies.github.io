9c437ab11edc53c6f0ce3880d40f3098b04c83da
fabztech <fabztech@outlook.com>
2022-11-27 14:07:09 -0500

    Implementacion de asincronismo con AXIOS, preview de imagen oculto cuando se sube la foto, modificacion CSS boton upload puppy

--- main.js
+++ main.js
@@ -9,15 +9,25 @@ const API_URL_UPLOAD = "https://api.thedogapi.com/v1/images/upload";
 
 const content = document.querySelector("#content");
 const content2 = document.querySelector("#content2");
+const preview = document.getElementById("file-ip-1-preview");
 
 const btn = document.querySelector("#btn");
 
 btn.addEventListener("click", renderRandomPuppies);
 
+// <-- AXIOS CONFIG -->
+const api = axios.create({
+	baseURL: "https://api.thedogapi.com/v1",
+	headers: {
+		"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
+	},
+});
+
 async function renderRandomPuppies() {
-	const rawData = await fetch(API_URL_RANDOM);
-	const jsonData = await rawData.json();
-	const view = `${jsonData
+	// <-- WITH AXIOS -->
+
+	const { data, status } = await api.get("/images/search?limit=4");
+	const view = `${data
 		.map(
 			(randomPuppy) =>
 				`<div class="img-container">
@@ -27,57 +37,90 @@ async function renderRandomPuppies() {
 		)
 		.join("")}`;
 	content2.innerHTML = view;
+	console.log("Random puppies rendered");
+
+	// <-- WITH FETCH -->
+
+	// const rawData = await fetch(API_URL_RANDOM);
+	// const jsonData = await rawData.json();
+	// const view = `${jsonData
+	// 	.map(
+	// 		(randomPuppy) =>
+	// 			`<div class="img-container">
+	// 	<img src="${randomPuppy.url}" alt="foto puppy aleatorio" />
+	// 	<button onclick="addPuppieToFavorites('${randomPuppy.id}')" class="boton-like">Like ❤️</button>
+	// </div>`
+	// 	)
+	// 	.join("")}`;
+	// content2.innerHTML = view;
 }
 
 async function addPuppieToFavorites(imgId) {
-	const rawData = await fetch(API_URL_FAVORITES, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"X-API-KEY": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
-		},
-		body: JSON.stringify({
-			image_id: imgId,
-		}),
+	// <-- WITH AXIOS -->
+	const { data, status } = await api.post("/favourites", {
+		image_id: imgId,
 	});
+
 	renderFavoritePuppies();
+	console.log("Puppy added to favorites");
+
+	// <-- WITH FETCH -->
+
+	// const rawData = await fetch(API_URL_FAVORITES, {
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 		"X-API-KEY": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
+	// 	},
+	// 	body: JSON.stringify({
+	// 		image_id: imgId,
+	// 	}),
+	// });
+	// renderFavoritePuppies();
 }
 
 async function deletePuppieFromFavorites(favourite_id) {
-	const rawData = await fetch(API_URL_DELETE(favourite_id), {
-		method: "DELETE",
-		headers: {
-			"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
-		},
-	});
+	// <-- WITH AXIOS -->
+	const { data, status } = await api.delete(`/favourites/${favourite_id}`);
 
-	const jsonData = await rawData.json();
-	console.log(jsonData);
 	renderFavoritePuppies();
+	console.log("Puppy deleted from favorites");
+
+	// <-- WITH FETCH -->
+	// const rawData = await fetch(API_URL_DELETE(favourite_id), {
+	// 	method: "DELETE",
+	// 	headers: {
+	// 		"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
+	// 	},
+	// });
+
+	// const jsonData = await rawData.json();
+	// console.log(jsonData);
+	// renderFavoritePuppies();
 }
 
 async function loadFavoritePuppies() {
-	const rawData = await fetch(API_URL_FAVORITES, {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-			"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
-		},
-	});
-	const jsonData = await rawData.json();
-	console.log(jsonData);
+	// <-- WITH AXIOS -->
+	const res = await api.get("/favourites");
+	console.log(res.data);
+
+	// <-- WITH FETCH -->
+
+	// const rawData = await fetch(API_URL_FAVORITES, {
+	// 	method: "GET",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 		"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
+	// 	},
+	// });
+	// const jsonData = await rawData.json();
+	// console.log(jsonData);
 }
 
 async function renderFavoritePuppies() {
-	const rawData = await fetch(API_URL_FAVORITES, {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-			"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
-		},
-	});
-	const jsonData = await rawData.json();
-	let view = `${jsonData
+	// <-- WITH AXIOS -->
+	const { data, status } = await api.get("/favourites");
+	let view = `${data
 		.map(
 			(eachPuppie) =>
 				`<div class= "img-container">
@@ -87,53 +130,102 @@ async function renderFavoritePuppies() {
 		)
 		.join("")}`;
 	content.innerHTML = view;
+	console.log("Favorite puppies rendered");
+
+	// <-- WITH FETCH -->
+	// const rawData = await fetch(API_URL_FAVORITES, {
+	// 	method: "GET",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 		"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
+	// 	},
+	// });
+	// const jsonData = await rawData.json();
+	// let view = `${jsonData
+	// 	.map(
+	// 		(eachPuppie) =>
+	// 			`<div class= "img-container">
+	// 		<img id="img-fav" src="${eachPuppie.image.url}" alt="foto puppy favorito" />
+	// 		<button onclick="deletePuppieFromFavorites(${eachPuppie.id});deleteUploadedPuppies('${eachPuppie.image_id}')" class="boton-like">Unlike 💔</button>
+	// 		</div>`
+	// 	)
+	// 	.join("")}`;
+	// content.innerHTML = view;
 }
 
 async function uploadPuppy() {
+	// <-- WITH AXIOS -->
+
 	const puppyForm = document.getElementById("puppyForm");
 	const puppyFormData = new FormData(puppyForm);
 
-	console.log(puppyFormData.get("file"));
+	const res = await api.post("/images/upload", puppyFormData);
 
-	const resp = fetch(API_URL_UPLOAD, {
-		method: "POST",
-		headers: {
-			"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
-		},
-		body: puppyFormData,
-	});
+	addPuppieToFavorites(res.data.id);
+	preview.style.display = "none";
+	console.log("Your puppy was uploaded");
 
-	const rawData = await resp;
-	const jsonData = await rawData.json();
+	// <-- WITH FETCH -->
 
-	console.log(jsonData);
+	// const puppyForm = document.getElementById("puppyForm");
+	// const puppyFormData = new FormData(puppyForm);
 
-	addPuppieToFavorites(jsonData.id);
+	// console.log(puppyFormData.get("file"));
+
+	// const resp = fetch(API_URL_UPLOAD, {
+	// 	method: "POST",
+	// 	headers: {
+	// 		"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
+	// 	},
+	// 	body: puppyFormData,
+	// });
+
+	// const rawData = await resp;
+	// const jsonData = await rawData.json();
+
+	// console.log(jsonData);
+
+	// addPuppieToFavorites(jsonData.id);
 }
 
 async function uploadedPuppies() {
-	const rawData = await fetch("https://api.thedogapi.com/v1/images?limit=10", {
-		headers: {
-			"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
-		},
-	});
-	const jsonData = await rawData.json();
-	console.log(jsonData);
+	// <-- WITH AXIOS BASE -->
+	const res = await api("/images");
+	console.log(res.data);
+
+	// <-- WITH FETCH -->
+	// const rawData = await fetch("https://api.thedogapi.com/v1/images?limit=10", {
+	// 	headers: {
+	// 		"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
+	// 	},
+	// });
+	// const jsonData = await rawData.json();
+	// console.log(jsonData);
 }
 
 async function deleteUploadedPuppies(imgId) {
-	const rawData = await fetch(`https://api.thedogapi.com/v1/images/${imgId}`, {
-		method: "DELETE",
-		headers: {
-			"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
-		},
+	// <-- WITH AXIOS BASE -->
+
+	const res = api({
+		method: "delete",
+		url: `/images/${imgId}`,
 	});
+
+	console.log("The Puppy removed from the API");
+
+	// <-- WITH FETCH -->
+
+	// const rawData = await fetch(`https://api.thedogapi.com/v1/images/${imgId}`, {
+	// 	method: "DELETE",
+	// 	headers: {
+	// 		"x-api-key": "live_fEQTW9cdqcD8Zbj6LSL5UjMs4XBT6DdzaFNUienaGAI81nOIURATazEUkacciDBH",
+	// 	},
+	// });
 }
 
 function showPreview(event) {
 	if (event.target.files.length > 0) {
-		var src = URL.createObjectURL(event.target.files[0]);
-		var preview = document.getElementById("file-ip-1-preview");
+		const src = URL.createObjectURL(event.target.files[0]);
 		preview.src = src;
 		preview.style.display = "block";
 	}
